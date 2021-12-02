c2caa673e5773d153b14155c44ec78b1827be4f0
fahadshoukat <fraza6970@gmail.com>
2021-12-02 22:04:05 +0500

    addingExectPositionofAnElement

--- scrollBar.js
+++ scrollBar.js
@@ -47,3 +47,33 @@ window.addEventListener('scroll', function(){
     }
 });
 
+const scrollLinks = document.querySelectorAll('.scroll');
+
+scrollLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+
+        const navHeight = navBar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navBar.classList.contains("fixed-nav");
+
+        let position = element.offsetTop - navHeight;
+
+        if(!fixedNav){
+            position = position - navHeight;
+        }
+        if(navHeight > 82){
+            position = position + containerHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+
+        linksContainer.style.height = 0;
+    });
+})
