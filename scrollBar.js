cde0c0de9c3832b52a33957de03af8be6a85557e
fahadshoukat <fraza6970@gmail.com>
2021-12-02 21:07:32 +0500

    addingToggleFunctionalityOnNavBar

--- scrollBar.js
+++ scrollBar.js
@@ -1,3 +1,23 @@
 const date = document.querySelector(".date");
 
 date.innerHTML = new Date().getFullYear();
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+toggleBtn.addEventListener('click', function(){
+
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    
+    if (containerHeight === 0) {
+        
+        linksContainer.style.height = `${linksHeight}px`;
+    }
+    else{
+        
+        linksContainer.style.height = 0;
+    }
+
+});
